9c9cae1fd045feac9c848ed47f8a5b8291fff98e
Aleksandr Nenasev <nenashev88@gmail.com>
2025-12-02 01:50:58 +0400

    fix catalog and category

--- catalog/controller/extension/module/category.php
+++ catalog/controller/extension/module/category.php
@@ -1,64 +1,80 @@
 <?php
 class ControllerExtensionModuleCategory extends Controller {
-	public function index() {
-		$this->load->language('extension/module/category');
+    public function index() {
+        $this->load->language('extension/module/category');
 
-		if (isset($this->request->get['path'])) {
-			$parts = explode('_', (string)$this->request->get['path']);
-		} else {
-			$parts = array();
-		}
+        if (isset($this->request->get['path'])) {
+            $parts = explode('_', (string)$this->request->get['path']);
+        } else {
+            $parts = array();
+        }
 
-		if (isset($parts[0])) {
-			$data['category_id'] = $parts[0];
-		} else {
-			$data['category_id'] = 0;
-		}
+        if (isset($parts[0])) {
+            $data['category_id'] = $parts[0];
+        } else {
+            $data['category_id'] = 0;
+        }
 
-		if (isset($parts[1])) {
-			$data['child_id'] = $parts[1];
-		} else {
-			$data['child_id'] = 0;
-		}
+        if (isset($parts[1])) {
+            $data['child_id'] = $parts[1];
+        } else {
+            $data['child_id'] = 0;
+        }
 
-		$this->load->model('catalog/category');
+        $this->load->model('catalog/category');
+        $this->load->model('catalog/product');
+        $this->load->model('tool/image'); // Добавляем загрузку модели изображений
 
-		$this->load->model('catalog/product');
+        $data['categories'] = array();
 
-		$data['categories'] = array();
+        $categories = $this->model_catalog_category->getCategories(0);
 
-		$categories = $this->model_catalog_category->getCategories(0);
+        foreach ($categories as $category) {
+            $children_data = array();
 
-		foreach ($categories as $category) {
-			$children_data = array();
+            if ($category['category_id'] == $data['category_id']) {
+                $children = $this->model_catalog_category->getCategories($category['category_id']);
 
-			if ($category['category_id'] == $data['category_id']) {
-				$children = $this->model_catalog_category->getCategories($category['category_id']);
+                foreach($children as $child) {
+                    $filter_data = array('filter_category_id' => $child['category_id'], 'filter_sub_category' => true);
 
-				foreach($children as $child) {
-					$filter_data = array('filter_category_id' => $child['category_id'], 'filter_sub_category' => true);
+                    // Добавляем изображение для дочерней категории
+                    if ($child['image']) {
+                        $image = $this->model_tool_image->resize($child['image'], 400, 400);
+                    } else {
+                        $image = $this->model_tool_image->resize('no_image.png', 100, 100);
+                    }
 
-					$children_data[] = array(
-						'category_id' => $child['category_id'],
-						'name' => $child['name'] . ($this->config->get('config_product_count') ? ' (' . $this->model_catalog_product->getTotalProducts($filter_data) . ')' : ''),
-						'href' => $this->url->link('product/category', 'path=' . $category['category_id'] . '_' . $child['category_id'])
-					);
-				}
-			}
+                    $children_data[] = array(
+                        'category_id' => $child['category_id'],
+                        'name' => $child['name'] . ($this->config->get('config_product_count') ? ' (' . $this->model_catalog_product->getTotalProducts($filter_data) . ')' : ''),
+                        'href' => $this->url->link('product/category', 'path=' . $category['category_id'] . '_' . $child['category_id']),
+                        'image' => $image // Добавляем изображение
+                    );
+                }
+            }
 
-			$filter_data = array(
-				'filter_category_id'  => $category['category_id'],
-				'filter_sub_category' => true
-			);
+            $filter_data = array(
+                'filter_category_id'  => $category['category_id'],
+                'filter_sub_category' => true
+            );
 
-			$data['categories'][] = array(
-				'category_id' => $category['category_id'],
-				'name'        => $category['name'] . ($this->config->get('config_product_count') ? ' (' . $this->model_catalog_product->getTotalProducts($filter_data) . ')' : ''),
-				'children'    => $children_data,
-				'href'        => $this->url->link('product/category', 'path=' . $category['category_id'])
-			);
-		}
+            // Добавляем изображение для основной категории
+            if ($category['image']) {
+                $image = $this->model_tool_image->resize($category['image'], 400, 400);
+            } else {
+                $image = $this->model_tool_image->resize('no_image.png', 100, 100);
+            }
 
-		return $this->load->view('extension/module/category', $data);
-	}
+            $data['categories'][] = array(
+                'category_id' => $category['category_id'],
+                'name'        => $category['name'] . ($this->config->get('config_product_count') ? ' (' . $this->model_catalog_product->getTotalProducts($filter_data) . ')' : ''),
+                'children'    => $children_data,
+                'href'        => $this->url->link('product/category', 'path=' . $category['category_id']),
+                'image'       => $image // Добавляем изображение
+            );
+        }
+
+        return $this->load->view('extension/module/category', $data);
+    }
 }
